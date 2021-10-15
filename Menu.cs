1970d3b821c96330d405a10ec1fca4b3270f6c57
Eduardo <eduardomendoncabraga@gmail.com>
2021-10-14 21:22:31 -0300

    Iniciando o projeto

---
+++ Menu.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace EditorHtml
+{
+  public class Menu
+  {
+    public static void Show()
+    {
+      Console.Clear();
+      Console.BackgroundColor = ConsoleColor.Blue;
+      Console.ForegroundColor = ConsoleColor.Black;
+      Console.WriteLine("Teste");
+    }
+  }
+}
